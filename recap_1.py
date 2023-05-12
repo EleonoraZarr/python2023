a3a09782ff798324268a88c18a3eb5faf25d93f8
EleonoraZarr <you@example.com>
2023-05-12 11:19:47 +0200

    modifica

--- recap_1.py
+++ recap_1.py
@@ -1,18 +1,18 @@
 """data una stringa creae un dizionario con chiavi le lettere della stringa 
 e come valori il numero di volte che la lettera appare nella stringa"""
 
-dizionario = {}
+dizionario = {}          #creo un dizionario vuoto
 
 print('Inserire una stringa:  ')
 parola = input(' ')
 
-for lettera in parola:
-    if lettera not in dizionario:  
-        dizionario[lettera]=1
+for lettera in parola:                      #scorro le lettere della parola
+    if lettera not in dizionario:          # se la lettera non è nel dizionario
+        dizionario[lettera]=1              # la aggiungo con valore 1
     else:
-        dizionario[lettera] += 1 
+        dizionario[lettera] += 1             # altrimenti aumento il valore
 
-print(dizionario)
+print(dizionario)                             # stampo il dizionario
 
-"""for chiave,valore in dizionario.items():   
+"""for chiave,valore in dizionario.items():            stampa chiave e valore e non il dizionario
   print(chiave, valore) """
